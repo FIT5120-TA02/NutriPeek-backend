2e0c02c916b0dde0c5b3cbb38195ee85d40e0abe
YXY1017 <jyii0007@student.monash.edu>
2025-04-08 09:39:50 +1000

    Include yolo food prediction router into v1 API

--- src/app/api/v1/__init__.py
+++ src/app/api/v1/__init__.py
@@ -3,8 +3,10 @@
 from fastapi import APIRouter
 
 from src.app.api.v1.health import router as health_router
+from src.app.api.v1.yolo_food_model import router as yolo_food_router
 
 api_router = APIRouter()
 
 # Include all routers
 api_router.include_router(health_router, prefix="/health", tags=["health"])
+api_router.include_router(yolo_food_router, prefix="/food", tags=["food"])
