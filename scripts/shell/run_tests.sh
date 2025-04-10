18e5ff17467340e8a9e74ff876843b21aec30641
jaden-fetchpet <jaden@fetchpet.au>
2025-04-10 18:44:09 +1000

    chore: update run test script

--- scripts/shell/run_tests.sh
+++ scripts/shell/run_tests.sh
@@ -6,5 +6,5 @@ set -e
 # Activate virtual environment
 source venv/bin/activate
 
-# Run tests
-pytest -v
+# Run tests with environment variables from test.env
+export $(grep -v "^#" .env | xargs) && python -m pytest -v
